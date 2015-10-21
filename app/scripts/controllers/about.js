086b5b06bd40ff82157da30a12646331cf60a119
handcraftjs <handcraft.js@gmail.com>
2015-10-21 14:42:57 +0900

    첫번째 커밋

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myhomeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myhomeApp
+ */
+angular.module('myhomeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myhomeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myhomeApp
+ */
+angular.module('myhomeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
